a9bd83ba62fb8cfed8e5a6b1db44f09c40966dec
yamari8666 <ryo082@gmail.com>
2015-10-06 18:21:09 +0900

    いじれるところまで

--- Assets/Resources/Scripts/GameManager.cs
+++ Assets/Resources/Scripts/GameManager.cs
@@ -2,14 +2,28 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
+	[SerializeField]
+	GameObject[] cubes = new GameObject[3];
+	[SerializeField]
+	GameObject spawnPoint;
 
+	Vector3 spawnPos;
+	int cubeType;
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		spawnPos = spawnPoint.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	if (Input.GetMouseButtonUp (0))
+		{
+			Vector3 cubeBirthPoint = new Vector3(Random.Range(spawnPos.x -2.5f,spawnPos.x+2.5f),spawnPos.y,Random.Range(spawnPos.z -2.5f,spawnPos.z+2.5f));
+			cubeType = Random.Range(0,3);
+			Vector3 cubeRot = new Vector3(Random.Range(-180,180),Random.Range(-180,180),Random.Range(-180,180));
+			GameObject c = Instantiate(cubes[cubeType],cubeBirthPoint,Quaternion.Euler(cubeRot))as GameObject;
+			c.AddComponent<CubeOperator>();
+		}
 	}
 }
